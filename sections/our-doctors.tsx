07f5b4577f8f098266c55bd1593885b69e32e647
Shajahan <shajansheriff@gmail.com>
2022-08-28 14:37:51 +0530

    add doctors json

---
+++ sections/our-doctors.tsx
@@ -0,0 +1,414 @@
+export function OurDoctors() {
+  return (
+    <section className="container pt-xl-2 pb-5 mb-md-3 mb-lg-5">
+      <div className="d-md-flex align-items-center justify-content-between text-center text-md-start pb-1 pb-lg-0 mb-4 mb-lg-5">
+        <h2 className="h1 mb-md-0">Qualified Medical Specialists</h2>
+        <a href="#" className="btn btn-outline-primary">
+          See all doctors
+          <i className="bx bx-right-arrow-alt fs-xl ms-2 me-n1"></i>
+        </a>
+      </div>
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+        <div className="col">
+          <div className="card card-hover border-0 bg-transparent">
+            <div className="position-relative">
+              <img
+                src="assets/img/team/16.jpg"
+                className="rounded-3"
+                alt="Dr. Ronald Richards"
+              />
+              <div className="card-img-overlay d-flex flex-column align-items-center justify-content-center rounded-3">
+                <span className="position-absolute top-0 start-0 w-100 h-100 bg-primary opacity-35 rounded-3"></span>
+                <div className="position-relative d-flex zindex-2">
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-facebook btn-sm bg-white me-2"
+                  >
+                    <i className="bx bxl-facebook"></i>
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-linkedin btn-sm bg-white me-2"
+                  >
+                    <i className="bx bxl-linkedin"></i>
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-twitter btn-sm bg-white"
+                  >
+                    <i className="bx bxl-twitter"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="card-body text-center p-3">
+              <h3 className="fs-lg fw-semibold pt-1 mb-2">
+                Dr. Ronald Richards
+              </h3>
+              <p className="fs-sm mb-2">Neurosurgeon</p>
+              <div className="d-flex align-items-center justify-content-center">
+                <div className="text-nowrap me-1">
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                </div>
+                <span className="fs-xs text-muted">(19 reviews)</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card card-hover border-0 bg-transparent">
+            <div className="position-relative">
+              <img
+                src="assets/img/team/17.jpg"
+                className="rounded-3"
+                alt="Dr. Esther Howard"
+              />
+              <div className="card-img-overlay d-flex flex-column align-items-center justify-content-center rounded-3">
+                <span className="position-absolute top-0 start-0 w-100 h-100 bg-primary opacity-35 rounded-3"></span>
+                <div className="position-relative d-flex zindex-2">
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-facebook btn-sm bg-white me-2"
+                  >
+                    <i className="bx bxl-facebook"></i>
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-linkedin btn-sm bg-white me-2"
+                  >
+                    <i className="bx bxl-linkedin"></i>
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-twitter btn-sm bg-white"
+                  >
+                    <i className="bx bxl-twitter"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="card-body text-center p-3">
+              <h3 className="fs-lg fw-semibold pt-1 mb-2">Dr. Esther Howard</h3>
+              <p className="fs-sm mb-2">Therapist</p>
+              <div className="d-flex align-items-center justify-content-center">
+                <div className="text-nowrap me-1">
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                </div>
+                <span className="fs-xs text-muted">(4 reviews)</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card card-hover border-0 bg-transparent">
+            <div className="position-relative">
+              <img
+                src="assets/img/team/18.jpg"
+                className="rounded-3"
+                alt="Dr. Jerome Bell"
+              />
+              <div className="card-img-overlay d-flex flex-column align-items-center justify-content-center rounded-3">
+                <span className="position-absolute top-0 start-0 w-100 h-100 bg-primary opacity-35 rounded-3"></span>
+                <div className="position-relative d-flex zindex-2">
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-facebook btn-sm bg-white me-2"
+                  >
+                    <i className="bx bxl-facebook"></i>
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-linkedin btn-sm bg-white me-2"
+                  >
+                    <i className="bx bxl-linkedin"></i>
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-twitter btn-sm bg-white"
+                  >
+                    <i className="bx bxl-twitter"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="card-body text-center p-3">
+              <h3 className="fs-lg fw-semibold pt-1 mb-2">Dr. Jerome Bell</h3>
+              <p className="fs-sm mb-2">Anesthesiologist</p>
+              <div className="d-flex align-items-center justify-content-center">
+                <div className="text-nowrap me-1">
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bx-star text-muted fs-sm opacity-75"></i>
+                </div>
+                <span className="fs-xs text-muted">(12 reviews)</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card card-hover border-0 bg-transparent">
+            <div className="position-relative">
+              <img
+                src="assets/img/team/19.jpg"
+                className="rounded-3"
+                alt="Dr. Ralph Edwards"
+              />
+              <div className="card-img-overlay d-flex flex-column align-items-center justify-content-center rounded-3">
+                <span className="position-absolute top-0 start-0 w-100 h-100 bg-primary opacity-35 rounded-3"></span>
+                <div className="position-relative d-flex zindex-2">
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-facebook btn-sm bg-white me-2"
+                  >
+                    <i className="bx bxl-facebook"></i>
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-linkedin btn-sm bg-white me-2"
+                  >
+                    <i className="bx bxl-linkedin"></i>
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-twitter btn-sm bg-white"
+                  >
+                    <i className="bx bxl-twitter"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="card-body text-center p-3">
+              <h3 className="fs-lg fw-semibold pt-1 mb-2">Dr. Ralph Edwards</h3>
+              <p className="fs-sm mb-2">Surgeon</p>
+              <div className="d-flex align-items-center justify-content-center">
+                <div className="text-nowrap me-1">
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                </div>
+                <span className="fs-xs text-muted">(8 reviews)</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card card-hover border-0 bg-transparent">
+            <div className="position-relative">
+              <img
+                src="assets/img/team/20.jpg"
+                className="rounded-3"
+                alt="Dr. Darrell Steward"
+              />
+              <div className="card-img-overlay d-flex flex-column align-items-center justify-content-center rounded-3">
+                <span className="position-absolute top-0 start-0 w-100 h-100 bg-primary opacity-35 rounded-3"></span>
+                <div className="position-relative d-flex zindex-2">
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-facebook btn-sm bg-white me-2"
+                  >
+                    <i className="bx bxl-facebook"></i>
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-linkedin btn-sm bg-white me-2"
+                  >
+                    <i className="bx bxl-linkedin"></i>
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-twitter btn-sm bg-white"
+                  >
+                    <i className="bx bxl-twitter"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="card-body text-center p-3">
+              <h3 className="fs-lg fw-semibold pt-1 mb-2">
+                Dr. Darrell Steward
+              </h3>
+              <p className="fs-sm mb-2">Cardiologist</p>
+              <div className="d-flex align-items-center justify-content-center">
+                <div className="text-nowrap me-1">
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bx-star text-muted fs-sm opacity-75"></i>
+                </div>
+                <span className="fs-xs text-muted">(14 reviews)</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card card-hover border-0 bg-transparent">
+            <div className="position-relative">
+              <img
+                src="assets/img/team/21.jpg"
+                className="rounded-3"
+                alt="Dr. Annette Black"
+              />
+              <div className="card-img-overlay d-flex flex-column align-items-center justify-content-center rounded-3">
+                <span className="position-absolute top-0 start-0 w-100 h-100 bg-primary opacity-35 rounded-3"></span>
+                <div className="position-relative d-flex zindex-2">
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-facebook btn-sm bg-white me-2"
+                  >
+                    <i className="bx bxl-facebook"></i>
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-linkedin btn-sm bg-white me-2"
+                  >
+                    <i className="bx bxl-linkedin"></i>
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-twitter btn-sm bg-white"
+                  >
+                    <i className="bx bxl-twitter"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="card-body text-center p-3">
+              <h3 className="fs-lg fw-semibold pt-1 mb-2">Dr. Annette Black</h3>
+              <p className="fs-sm mb-2">Pediatrician</p>
+              <div className="d-flex align-items-center justify-content-center">
+                <div className="text-nowrap me-1">
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bx-star text-muted fs-sm opacity-75"></i>
+                </div>
+                <span className="fs-xs text-muted">(10 reviews)</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card card-hover border-0 bg-transparent">
+            <div className="position-relative">
+              <img
+                src="assets/img/team/22.jpg"
+                className="rounded-3"
+                alt="Dr. Dianne Russell"
+              />
+              <div className="card-img-overlay d-flex flex-column align-items-center justify-content-center rounded-3">
+                <span className="position-absolute top-0 start-0 w-100 h-100 bg-primary opacity-35 rounded-3"></span>
+                <div className="position-relative d-flex zindex-2">
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-facebook btn-sm bg-white me-2"
+                  >
+                    <i className="bx bxl-facebook"></i>
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-linkedin btn-sm bg-white me-2"
+                  >
+                    <i className="bx bxl-linkedin"></i>
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-twitter btn-sm bg-white"
+                  >
+                    <i className="bx bxl-twitter"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="card-body text-center p-3">
+              <h3 className="fs-lg fw-semibold pt-1 mb-2">
+                Dr. Dianne Russell
+              </h3>
+              <p className="fs-sm mb-2">Dentist</p>
+              <div className="d-flex align-items-center justify-content-center">
+                <div className="text-nowrap me-1">
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                </div>
+                <span className="fs-xs text-muted">(5 reviews)</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card card-hover border-0 bg-transparent">
+            <div className="position-relative">
+              <img
+                src="assets/img/team/23.jpg"
+                className="rounded-3"
+                alt="Dr. Courtney Henry"
+              />
+              <div className="card-img-overlay d-flex flex-column align-items-center justify-content-center rounded-3">
+                <span className="position-absolute top-0 start-0 w-100 h-100 bg-primary opacity-35 rounded-3"></span>
+                <div className="position-relative d-flex zindex-2">
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-facebook btn-sm bg-white me-2"
+                  >
+                    <i className="bx bxl-facebook"></i>
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-linkedin btn-sm bg-white me-2"
+                  >
+                    <i className="bx bxl-linkedin"></i>
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-icon btn-secondary btn-twitter btn-sm bg-white"
+                  >
+                    <i className="bx bxl-twitter"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="card-body text-center p-3">
+              <h3 className="fs-lg fw-semibold pt-1 mb-2">
+                Dr. Courtney Henry
+              </h3>
+              <p className="fs-sm mb-2">Gynecologist</p>
+              <div className="d-flex align-items-center justify-content-center">
+                <div className="text-nowrap me-1">
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                  <i className="bx bxs-star text-warning fs-sm"></i>
+                </div>
+                <span className="fs-xs text-muted">(16 reviews)</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
